a7f1e829d609a786a554d4cc841f827cc41191b8
warnerej <warnerej@mail.uc.edu>
2026-09-15 17:47:39 -0400

    Added Search By ID
    Added the search by ID to Vocab

--- backend/app/routers/vocab.py
+++ backend/app/routers/vocab.py
@@ -9,3 +9,7 @@ router = APIRouter(prefix="/vocab", tags=["Vocab"])
 @router.get("/", response_model=List[VocabResponse])
 def get_vocab():
   return VocabService.get_vocab()
+
+@router.get("/{id}", response_model=List[VocabResponse])
+def get_vocab_by_id(id: int):
+  return VocabService.get_vocab_by_id(id)
--- backend/app/services/vocab_service.py
+++ backend/app/services/vocab_service.py
@@ -1,11 +1,15 @@
 # services/user_service.py
 from database import get_supabase
-from models import VocabResponse
 
 supabase = get_supabase()
 
 class VocabService:
     @staticmethod
     def get_vocab():
-            response = (supabase.table("vocab").select("*").limit(10).execute())
-            return response.data
+        response = (supabase.table("vocab").select("*").limit(10).execute())
+        return response.data
+
+    @staticmethod
+    def get_vocab_by_id(id: int):
+        response = (supabase.table("vocab").select("*").eq("id", id).limit(10).execute())
+        return response.data
